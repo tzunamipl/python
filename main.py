ebb69534281efe65baea7d21fb5b709194a9171d
kpoznanski <poznanski.krzysztof@gmail.com>
2025-01-31 09:57:37 +0100

    Hello function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-"Hello world"
+#user_name = input("What is your name? ")
+#ser_age = input("How old are you? ")
+
+#print("Hello " + user_name + ", you are " + user_age)
+
+def say_hello(user_name):
+    print("Hello " + user_name + "!")
+    print("hELLO1")
+
+say_hello("Dupa")
+say_hello("Dddduupaaa")
+say_hello("Dudu")
