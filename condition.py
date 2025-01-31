21bc238af3fc1ca23128e0b65be42d7cc64bab84
kpoznanski <poznanski.krzysztof@gmail.com>
2025-01-31 12:54:06 +0100

    If condition

---
+++ condition.py
@@ -0,0 +1,12 @@
+user_age = input("How old are you? ")
+month = "July"
+
+if int(user_age) >= 18:
+    print("You're officaially an adult")
+else:
+    print("You're not adult yet")
+
+if month in ["June","July","August"]:
+    print("It's summer!")
+
+print("End of orogram")
