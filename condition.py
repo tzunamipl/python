94c0d8bd4a98519425b3401a132128bd31151b55
kpoznanski <poznanski.krzysztof@gmail.com>
2025-01-31 12:56:11 +0100

    elif

--- condition.py
+++ condition.py
@@ -1,8 +1,10 @@
 user_age = input("How old are you? ")
 month = "July"
 
-if int(user_age) >= 18:
+if int(user_age) >= 25:
     print("You're officaially an adult")
+elif int(user_age) >= 18:
+    print("You are young adult")
 else:
     print("You're not adult yet")
 
