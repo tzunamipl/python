30dbdf812d316651c245d0f7091fe8dfc4352444
kpoznanski <poznanski.krzysztof@gmail.com>
2025-02-06 11:30:41 +0100

    class in module

--- oop.py
+++ oop.py
@@ -1,21 +1,8 @@
-class Robot:
-    def __init__(self, name, version_number):
-        self.name = name
-        self.version_number = version_number
-        self.internal_temperature = 39.0
-
-    def say_hi(self):
-        print("Hello, my name is " + self.name + ", ready to help!")
-
-    def init_hardware(self):
-        print("Init...")
-
-    def print_info(self):
-        self.say_hi()
-        print("Version number: " + str(self.version_number))
-        print("TEmperature: " + str(self.internal_temperature))
+from robot import Robot
 
 print("test")
 my_robot = Robot("R2D2", 1)
+my_robot_2 = Robot("C3PO", "2")
 my_robot.say_hi()
 my_robot.print_info()
+my_robot_2.print_info()
