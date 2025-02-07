b3b9d71cd13c882027614a48655b54ee7f89f21b
kpoznanski <poznanski.krzysztof@gmail.com>
2025-02-07 10:48:11 +0100

    Progress

--- oop.py
+++ oop.py
@@ -1,4 +1,5 @@
 from robot import Robot
+from robot import RoboticArm
 
 print("test")
 my_robot = Robot("R2D2", 1)
@@ -6,3 +7,7 @@ my_robot_2 = Robot("C3PO", "2")
 my_robot.say_hi()
 my_robot.print_info()
 my_robot_2.print_info()
+
+robot_arm = RoboticArm("Bob", 1, 20)
+robot_arm.print_info()
+robot_arm.pick_object(20, 30)
--- robot.py
+++ robot.py
@@ -1,3 +1,5 @@
+import time
+
 class Robot:
     def __init__(self, name, version_number):
         self.name = name
@@ -8,9 +10,32 @@ class Robot:
         print("Hello, my name is " + self.name + ", ready to help!")
 
     def init_hardware(self):
-        print("Init...")
+        print("Initializinag, please wait...")
+        time.sleep(0.5)
+        print("20%", end="\r")
+        time.sleep(1.5)
+        print("50%", end="\r")
+        time.sleep(2)
+        print("90%", end="\r")
+        time.sleep(0.5)
+        print("95%", end="\r")
+        time.sleep(0.25)
+        print("100%")
+        time.sleep(0.25)
+        print("Done!")
 
     def print_info(self):
+        self.init_hardware()
         self.say_hi()
         print("Version number: " + str(self.version_number))
-        print("TEmperature: " + str(self.internal_temperature))
+        print("Temperature: " + str(self.internal_temperature))
+        print("\n")
+
+class RoboticArm(Robot):
+    def __init__(self, name, version_number, reach):
+        super().__init__(name, version_number)
+        self.reach = reach
+
+    def pick_object(self, x, y):
+        self.init_hardware()
+        print("Pick object at (" + str(x) + ", " + str(y) + "), robot needs to be at (" + str(x + self.reach) + ", " + str(y) + ").")
