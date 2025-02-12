bb98fd15227ff0a41ba9bfe6c2011dbbf6ad60fb
kpoznanski <poznanski.krzysztof@gmail.com>
2025-02-12 13:11:06 +0100

    New training

---
+++ PCEP/script.py
@@ -0,0 +1,20 @@
+name = "dupa"
+
+print(name)
+
+total = 100
+a = total + 10
+print(a)
+
+a, b, c = 1, 2, 3
+print(str(a) + str(b) + str(c))
+
+text_1 = "Dupa" \
+    "Duuuuppppa" \
+    "Dupa"
+print(text_1)
+
+text_2 = """Linia 1
+Linia 2
+Linia 3"""
+print(text_2)
