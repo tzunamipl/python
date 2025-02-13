cf14a03ec1e15ca8f77c2037038e7eb966fc8d99
kpoznanski <poznanski.krzysztof@gmail.com>
2025-02-13 09:59:03 +0100

    Strings

---
+++ PCEP/strings.py
@@ -0,0 +1,23 @@
+a = "Python"
+b = "Monty"
+print(len(a))
+print(a[3])
+
+i = 0
+length = len(a)
+while i < length:
+    print(a[i], end=",")
+    i += 1
+
+i = len(a) - 1
+while i >= 0:
+    print(a[i], end="")
+    i -= 1
+
+print(a[1:4:1])
+print(a[1:5:3])
+
+s1 = b + " "+ a
+print(s1)
+s2 = a * 2
+print(s2)
