c017f4ab39688b1c5c7396127b314ebed1e08667
kpoznanski <poznanski.krzysztof@gmail.com>
2025-04-15 14:47:58 +0200

    info class renamed

--- oop.py
+++ oop.py
@@ -4,8 +4,8 @@ from classes.robot import RoboticArm
 print("test")
 my_robot = Robot("R2D2", 1)
 my_robot_2 = Robot("C3PO", "2")
-my_robot.say_hi()
-my_robot.print_info()
+my_robot.say_hello()
+my_robot.print_infos()
 my_robot_2.print_info()
 
 robot_arm = RoboticArm("Bob", 1, 20)
